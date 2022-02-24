c5517cca24f4d375ebb950d396448e65c16dff25
jame123785656 <61955070+jame123785656@users.noreply.github.com>
2022-02-24 22:08:29 +0700

    update index_admin

--- app/Views/component/navbar_admin.php
+++ app/Views/component/navbar_admin.php
@@ -6,8 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <link href="./css/navbar.css" rel="stylesheet" />
-    <title>Document</title>
+    
 </head>
 
 <body>
