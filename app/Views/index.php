1af63cc274a2279db297023c72b969af3469e97a
jame123785656 <61955070+jame123785656@users.noreply.github.com>
2022-03-08 22:50:32 +0700

    แก้ index

--- app/Views/index.php
+++ app/Views/index.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <link href="./css/index.css" rel="stylesheet" />
   <link href="./css/navbar.css" rel="stylesheet" />
-  <title>TC Aren</title>
+  <title>TC Arena</title>
 </head>
 
 <body>
